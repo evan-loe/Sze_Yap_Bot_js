a3cb266bc07ad937cee2b6ff22f9bbe3b28cf841
evan-loe <evan.loe@mail.utoronto.ca>
2022-05-29 21:32:58 -0400

    Added thing and cheese to my app

--- src/search/importDictionary.js
+++ src/search/importDictionary.js
@@ -4,6 +4,8 @@ const path = require("path");
 const dfd = require("danfojs-node");
 const config = require("../config/config.json");
 
+const thing = "cheese"
+
 module.exports = dfd.read_csv(
   path.join(__dirname, `../assets/${config.useFeedback ? config.gc_feedbackFile : config.gc_file}`)
 );
